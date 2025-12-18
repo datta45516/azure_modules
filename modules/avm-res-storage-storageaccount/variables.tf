f46915e83803500e1c339ad3b1d7b7bd9eb443bc
datta45516 <sdattatray516@gmail.com>
2025-12-18 12:45:42 +0530

    Add variables for storage account configuration

---
+++ modules/avm-res-storage-storageaccount/variables.tf
@@ -0,0 +1,8 @@
+variable "name" { type = string }
+variable "resource_group_name" { type = string }
+variable "location" { type = string }
+variable "account_tier" { type = string default = "Standard" }
+variable "account_replication_type" { type = string default = "LRS" }
+variable "account_kind" { type = string default = "StorageV2" }
+variable "access_tier" { type = string default = "Hot" }
+variable "tags" { type = map(string) default = {} }
