06c2a8271ebe8ae5c5ff22347d5b09254ad115c2
datta45516 <sdattatray516@gmail.com>
2025-12-18 12:56:04 +0530

    Add variables for key vault module

---
+++ modules/avm-res-keyvault-vault/variables.tf
@@ -0,0 +1,7 @@
+variable "name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "tenant_id" { type = string }
+variable "sku_name" { type = string default = "standard" }
+variable "purge_protection_enabled" { type = bool default = false }
+variable "tags" { type = map(string) default = {} }
