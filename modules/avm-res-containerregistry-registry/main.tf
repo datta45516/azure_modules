635aeeab873d87cf85d14aa0a4ccf0bacab7957c
datta45516 <sdattatray516@gmail.com>
2025-12-18 16:57:50 +0530

    Add Terraform configuration for Azure Container Registry

---
+++ modules/avm-res-containerregistry-registry/main.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_container_registry" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+  admin_enabled       = var.admin_enabled
+  zone_redundancy_enabled = var.zone_redundancy_enabled
+
+  tags = var.tags
+}
