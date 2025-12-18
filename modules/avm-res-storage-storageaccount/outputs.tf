237a7fe16b10f82b301340893d011dea215934a8
datta45516 <sdattatray516@gmail.com>
2025-12-18 12:46:15 +0530

    Add outputs for storage account in outputs.tf

---
+++ modules/avm-res-storage-storageaccount/outputs.tf
@@ -0,0 +1,3 @@
+output "id" { value = azurerm_storage_account.this.id }
+output "primary_connection_string" { value = azurerm_storage_account.this.primary_connection_string sensitive = true }
+output "primary_blob_endpoint" { value = azurerm_storage_account.this.primary_blob_endpoint }
