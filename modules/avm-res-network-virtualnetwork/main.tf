a25fd4f08695947f119b0759edfbcb4230b25487
datta45516 <sdattatray516@gmail.com>
2025-12-18 12:15:14 +0530

    Create main.tf

---
+++ modules/avm-res-network-virtualnetwork/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_virtual_network" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  address_space       = var.address_space
+  dns_servers         = var.dns_servers
+  tags                = var.tags
+}
