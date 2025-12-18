26612e2602e7d75f1f49b0ab8c9b48d576137e8e
datta45516 <sdattatray516@gmail.com>
2025-12-18 13:13:02 +0530

    Add variables for managed cluster module

---
+++ modules/avm-res-containerservice-managedcluster/variables.tf
@@ -0,0 +1,7 @@
+variable "name" { type = string }
+variable "location" { type = string }
+variable "resource_group_name" { type = string }
+variable "dns_prefix" { type = string }
+variable "node_count" { type = number default = 1 }
+variable "vm_size" { type = string default = "Standard_DS2_v2" }
+variable "tags" { type = map(string) default = {} }
