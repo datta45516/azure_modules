c4a3b3feacbf9bd63a9d6bcb0256df25bb5de101
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:33:18 +0530

    Create main.tf

---
+++ modules/avm-res-logic-workflow/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_logic_app_workflow" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
