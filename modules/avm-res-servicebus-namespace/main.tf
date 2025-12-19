ad4864b78fde743f53e5971690d719b8d3b49d65
datta45516 <sdattatray516@gmail.com>
2025-12-19 12:38:56 +0530

    Create main.tf

---
+++ modules/avm-res-servicebus-namespace/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_servicebus_namespace" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku                 = var.sku
+  capacity            = var.capacity
+
+  tags = var.tags
+}
