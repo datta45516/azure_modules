ea5c32a0e089f1db292435a9562cd35772583317
datta45516 <sdattatray516@gmail.com>
2025-12-19 12:24:32 +0530

    Create main.tf

---
+++ modules/avm-res-network-privatednszone/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_private_dns_zone" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
