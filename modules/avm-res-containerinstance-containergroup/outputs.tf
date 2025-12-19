da9859a14c32fa8db1870e39d53c81a0460cc63b
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:19:57 +0530

    Create outputs.tf

---
+++ modules/avm-res-containerinstance-containergroup/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_container_group.this.id
+}
+
+output "ip_address" {
+  value = azurerm_container_group.this.ip_address
+}
+
+output "fqdn" {
+  value = azurerm_container_group.this.fqdn
+}
