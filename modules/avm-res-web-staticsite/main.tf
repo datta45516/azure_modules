555aa445ae8ed6dd13a710b00171c2d5e1a2ee0a
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:35:35 +0530

    Create main.tf

---
+++ modules/avm-res-web-staticsite/main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_static_site" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku_tier            = var.sku_tier
+  sku_size            = var.sku_size
+
+  tags = var.tags
+}
