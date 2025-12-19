f3ac2b0b3bd1f3987698d332a600c0bdbf62d25f
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:37:35 +0530

    Create variables.tf

---
+++ modules/avm-res-machinelearningservices-workspace/variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  type        = string
+}
+
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "application_insights_id" {
+  type        = string
+}
+
+variable "key_vault_id" {
+  type        = string
+}
+
+variable "storage_account_id" {
+  type        = string
+}
+
+variable "container_registry_id" {
+  type        = string
+  default     = null
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
