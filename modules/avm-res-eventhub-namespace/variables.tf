3acf80d7d1b76def742acd3a2faea80cf77ca94e
datta45516 <sdattatray516@gmail.com>
2025-12-19 12:41:43 +0530

    Create variables.tf

---
+++ modules/avm-res-eventhub-namespace/variables.tf
@@ -0,0 +1,33 @@
+variable "name" {
+  type        = string
+}
+
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "sku" {
+  type        = string
+  default     = "Standard"
+}
+
+variable "capacity" {
+  type        = number
+  default     = 1
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
+
+
+
+
+
+
+
