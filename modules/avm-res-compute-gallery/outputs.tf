f570a7c1dbfa98cc66c794acdbdc94bc611f11d1
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:24:25 +0530

    Create outputs.tf

---
+++ modules/avm-res-compute-gallery/outputs.tf
@@ -0,0 +1,8 @@
+output "id" {
+  value       = azurerm_shared_image_gallery.this.id
+  description = "Compute Gallery ID"
+}
+
+output "name" {
+  value       = azurerm_shared_image_gallery.this.name
+}
