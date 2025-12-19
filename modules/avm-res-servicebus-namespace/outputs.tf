b22d6c0f144464d151be04170b25fda3360888e7
datta45516 <sdattatray516@gmail.com>
2025-12-19 12:40:10 +0530

    Create outputs.tf

---
+++ modules/avm-res-servicebus-namespace/outputs.tf
@@ -0,0 +1,8 @@
+output "id" {
+  value = azurerm_servicebus_namespace.this.id
+}
+
+output "default_primary_connection_string" {
+  value     = azurerm_servicebus_namespace.this.default_primary_connection_string
+  sensitive = true
+}
