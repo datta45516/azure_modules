e0d95d7b60c99ec47e742fcc5982abed7d0671b8
datta45516 <sdattatray516@gmail.com>
2025-12-19 13:15:36 +0530

    Create main.tf

---
+++ modules/avm-res-network-ddosprotectionplan/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_network_ddos_protection_plan" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
