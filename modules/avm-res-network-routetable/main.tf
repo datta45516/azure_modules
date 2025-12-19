5e8bcf7162e51284658d458c5a38d57ce74572f9
datta45516 <sdattatray516@gmail.com>
2025-12-19 13:02:17 +0530

    Create main.tf

---
+++ modules/avm-res-network-routetable/main.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_route_table" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+
+  tags = var.tags
+}
