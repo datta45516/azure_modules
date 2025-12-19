1ef2e05054da57c5022eeaa421216c587cb6e09c
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:41:39 +0530

    Create main.tf

---
+++ modules/avm-res-network-dnsresolver/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_dns_resolver" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  virtual_network_id  = var.virtual_network_id
+
+  tags = var.tags
+}
