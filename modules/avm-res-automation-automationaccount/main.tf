ac35eac25909be0aff2be64615ae0b59eb8a42c6
datta45516 <sdattatray516@gmail.com>
2025-12-19 13:17:41 +0530

    Create main.tf

---
+++ modules/avm-res-automation-automationaccount/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_automation_account" "this" {
+  name                = var.name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  sku_name            = var.sku_name
+
+  tags = var.tags
+}
