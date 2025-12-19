3be9de58b4c8627bd2e841975d5bef3e7f88a842
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:38:11 +0530

    Create outputs.tf

---
+++ modules/avm-res-machinelearningservices-workspace/outputs.tf
@@ -0,0 +1,11 @@
+output "id" {
+  value = azurerm_machine_learning_workspace.this.id
+}
+
+output "workspace_id" {
+  value = azurerm_machine_learning_workspace.this.workspace_id
+}
+
+output "discovery_url" {
+  value = azurerm_machine_learning_workspace.this.discovery_url
+}
