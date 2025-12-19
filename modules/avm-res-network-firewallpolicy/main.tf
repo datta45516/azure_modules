0cece2d9dd550802c379c487cc6d08570f2c33e3
datta45516 <sdattatray516@gmail.com>
2025-12-19 14:20:43 +0530

    Create main.tf

---
+++ modules/avm-res-network-firewallpolicy/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_firewall_policy" "this" {
+  name                = var.name
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  sku                 = var.sku
+
+  tags = var.tags
+}
