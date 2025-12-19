4fe52d81f48bbcc13db7980f9bb21ad5cb36fa0a
datta45516 <sdattatray516@gmail.com>
2025-12-19 12:23:22 +0530

    Create variables.tf

---
+++ modules/avm-res-network-azurefirewall/variables.tf
@@ -0,0 +1,31 @@
+variable "name" {
+  type        = string
+}
+
+variable "location" {
+  type        = string
+}
+
+variable "resource_group_name" {
+  type        = string
+}
+
+variable "subnet_id" {
+  type        = string
+  description = "AzureFirewallSubnet ID"
+}
+
+variable "sku_name" {
+  type        = string
+  default     = "AZFW_VNet"
+}
+
+variable "sku_tier" {
+  type        = string
+  default     = "Standard"
+}
+
+variable "tags" {
+  type        = map(string)
+  default     = {}
+}
